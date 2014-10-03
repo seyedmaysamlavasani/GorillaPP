29b1d5d38563728cbb844d456e476cf973818820
Maysam Lavasani <maysam@lonewolf.ece.utexas.edu>
2014-10-03 10:25:50 -0500

    remove pipeline compiler

--- compiler/engineCompiler/pipelined/.scala
+++
@@ -1,6 +0,0 @@
-val wait = Reg(UFix(width = 32))	//Global variable 
-Reg inputReg(UFix(width = 32))
-Reg OutputReg(UFix(width = 32))
-val GS_INITIALIZE = UFix(1)
-val GS_COUNT = UFix(2)
-val GS_GET_INC_FACTOR = UFix(3)
--- compiler/engineCompiler/pipelined/genericHeader
+++
@@ -1,9 +0,0 @@
-package Tutorial
-
-import Chisel._
-import Node._
-import Literal._
-import scala.collection.mutable.HashMap
-import scala.collection.mutable.ArrayBuffer
-
-
